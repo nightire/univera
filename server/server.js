45e2a4659f81177c223bb3aa400a6f919d839c19
nightire <nightire@very-geek.com>
2016-04-07 21:55:30 +0800

    💀  Do not rely on process.env.package_name
    Signed-off-by: nightire <nightire@very-geek.com>

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ export const root = path.join(__dirname, '..');
 
 const server = new Koa();
 server.port = process.env.PORT || 3000;
-server.name = process.env.NAME = process.env.npm_package_name;
+server.name = process.env.NAME = require('../package.json').name;
 
 if ('production' != server.env) {
   server.use(require('koa-logger')());
@@ -32,7 +32,7 @@ server.use(favicon(path.join(root, 'public', 'favicon.ico')));
 server.use(routes);
 
 server.listen(server.port, () => {
-  console.info(`[${server.name}] ☞  http://localhost:${server.port}`); // eslint-disable-line
+  console.info(`[${server.name}] ☞ http://localhost:${server.port}`); // eslint-disable-line
 });
 
 export default server;
